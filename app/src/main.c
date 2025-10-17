97a201d0cbb46b70d515fe224b7b88299835f255
Green-Zinger <tristanjakob12@gmail.com>
2025-10-16 20:54:29 -0600

    Creation of a 4 bit binary counter using a button and 4 leds

--- app/src/main.c
+++ app/src/main.c
@@ -13,7 +13,10 @@
 #define SLEEP_TIME_MS 1
 
 int main(void){
-
+    int bit_0 = 0;
+    int bit_1 = 0;
+    int bit_2 = 0;
+    int bit_3 = 0;
     if(0>BTN_init()){
         return 0;
     }
@@ -24,9 +27,40 @@ int main(void){
 
     while(1){
         if(BTN_check_clear_pressed(BTN0)){
-            LED_toggle(LED0);
-            printk("Button 0 pressed!\n");
+            if(bit_0 == 0){
+                bit_0 = 1;
+                LED_toggle(LED0);
+            }else{
+                bit_0 = 0;
+                LED_toggle(LED0);
+                if(bit_1 == 0){
+                    bit_1 = 1;
+                    LED_toggle(LED1);
+                }else{
+                    bit_1 = 0;
+                    LED_toggle(LED1);
+                    if(bit_2 == 0){
+                        bit_2 = 1;
+                        LED_toggle(LED2);
+                    }else{
+                        bit_2 = 0;
+                        LED_toggle(LED2);
+                        if(bit_3 == 0){
+                            bit_3 = 1;
+                            LED_toggle(LED3);
+                        }else{
+                            bit_0 = 0;
+                            bit_1 = 0;
+                            bit_2 = 0;
+                            bit_3 = 0;
+                            LED_toggle(LED3);
+                        }
+                    }
+                }
+            }
+                 
         }
+        
         k_msleep(SLEEP_TIME_MS);
     }
     return 0;
