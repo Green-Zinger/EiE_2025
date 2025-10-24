a947dc7e0cd21bc61baaf37d147dba807a116d1f
Green-Zinger <tristanjakob12@gmail.com>
2025-10-23 19:15:47 -0600

    Setting up the state machine intially for lesson 5

--- app/src/main.c
+++ app/src/main.c
@@ -8,21 +8,17 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
-<<<<<<< HEAD
 #include <inttypes.h>
 #include "BTN.h"
 #include "LED.h"
-=======
 
 #include "BTN.h"
 #include "LED.h"
 
 #define SLEEP_MS 1
->>>>>>> 411e7ce96d2adce819f520352c905a74509f0652
 
 #define SLEEP_TIME_MS 1
 
-<<<<<<< HEAD
 int main(void){
 
     if(0>BTN_init()){
@@ -41,7 +37,6 @@ int main(void){
         k_msleep(SLEEP_TIME_MS);
     }
     return 0;
-=======
   if (0 > BTN_init()) {
     return 0;
   }
@@ -53,5 +48,4 @@ int main(void){
     k_msleep(SLEEP_MS);
   }
 	return 0;
->>>>>>> 411e7ce96d2adce819f520352c905a74509f0652
 }
