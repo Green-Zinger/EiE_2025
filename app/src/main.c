de7ff7dcca43412c3f5aa65dab9ce5616bc969af
Green-Zinger <tristanjakob12@gmail.com>
2025-11-11 17:10:49 -0700

    Fixed so it fit the requirement of a input password any size. This is the intial attempt.

--- app/src/main.c
+++ app/src/main.c
@@ -15,57 +15,91 @@
 #define PASSWORD_LENGTH 4
 
 int main(void){
-    int password[PASSWORD_LENGTH] = {1, 2, 3, 1};
-    int input[PASSWORD_LENGTH];
     if(0>BTN_init()){
+        printk("Failure setting up BTN")
         return 0;
     }
 
     if(0>LED_init()){
+        printk("Failure setting up LED")
         return 0;
     }
     
+    //Setting up intial condition for locked password state.
+    int input_cap;
+    int input_length;
+
+    int password[PASSWORD_LENGTH] = {0, 1, 1, 2};
+    int *input;
+    int locked = 1;
+    if(input == NULL)
+        printk("Memory error.");
+        return 0;
+    //The main loop
     while(1){
+        *input = k_malloc(input_cap*sizeof(int));
+        input_cap = PASSWORD_LENGTH;
+        input_length = 0;
         LED_toggle(LED0);
-        for(int i = 0; i<PASSWORD_LENGTH; i++){
+        //locked state loop
+        while(locked){
+            if(input_length==input_cap){
+                int *old = input;
+                input = k_malloc(input_cap*2*sizeof(int));
+
+                if(input == NULL)
+                    return 0;
+
+                for(int i =0; i<input_cap; i++)
+                    input[i] = old[i];
+                input_cap*=2;
+                free(old);
+            }
             while(1){
                 if(BTN_check_clear_pressed(BTN0)){
-                    input[i] = 0;
+                    input[input_length] = 0;
                     break;
-                }
-                else if(BTN_check_clear_pressed(BTN1)){
-                    input[i] = 1;
+                }else if(BTN_check_clear_pressed(BTN1)){
+                    input[input_length] = 1;
                     break;
-                }
-                else if(BTN_check_clear_pressed(BTN2)){
-                    input[i] = 2;
+                }else if(BTN_check_clear_pressed(BTN2)){
+                    input[input_length] = 2;
                     break;
-                }
-                else if(BTN_check_clear_pressed(BTN3)){
-                    input[i] = 3;
+                }else if(BTN_check_clear_pressed(BTN3)){
+                    locked = 0;
                     break;
                 }
                 k_msleep(SLEEP_TIME_MS);
             }
-            
+
         }
-        int i;
-        for(i = 0; i<PASSWORD_LENGTH; i++){
-            if(password[i]!=input[i]){
-                printk("Incorrect! You Suck!\n");
-                LED_toggle(LED0);
-                break;
+        //
+        LED_toggle(LED0);
+        free(input);
+        if(input_length != PASSWORD_LENGTH){
+            printk("Incorrect! You Suck!\n");   
+        }else{
+            for(int i = 0; i<PASSWORD_LENGTH; i++){
+                if(input[i] != password[i])
+                    printk("Incorrect! You Suck!\n");
+                    break; 
             }
-            printk(" The %dth thing is %d",i,input[i]);
+            if(i == PASSWORD_LENGTH)
+                printk("Correct! You are the Goat!\n");
         }
-        if(i == 4){
-            printk("Correct! You are the Goat!\n");
-            LED_toggle(LED0);
-        }
-        while(!BTN_check_pressed(BTN0)&&!BTN_check_pressed(BTN1)&&!BTN_check_pressed(BTN2)&&!BTN_check_pressed(BTN3)){
+        //This is the waiting state.
+        while(!locked){
+            if(BTN_check_clear_pressed(BTN0)){
+                locked = 1;
+            }else if(BTN_check_clear_pressed(BTN1)){
+                locked = 1;
+            }else if(BTN_check_clear_pressed(BTN2)){
+                locked = 1;
+            }else if(BTN_check_clear_pressed(BTN3)){
+                locked = 1;
+            }
             k_msleep(SLEEP_TIME_MS);
         }
-        
     }
     return 0;
 }
