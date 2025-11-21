1eec9f5c186d4a529df7f6bf75b80277d9e5134c
Green-Zinger <tristanjakob12@gmail.com>
2025-11-20 18:28:28 -0700

    This is the first challenge on lesson 5 using exit functions rather than entry functions.

--- app/src/my_state_machine.c
+++ app/src/my_state_machine.c
@@ -10,9 +10,9 @@
 /*----------------------------------------
 * Function Prototypes
 ------------------------------------------*/
-static void led_on_state_entry(void* o);
+static void led_on_state_exit(void* o);
 static enum smf_state_result led_on_state_run(void* o);
-static void led_off_state_entry(void* o);
+static void led_off_state_exit(void* o);
 static enum smf_state_result led_off_state_run(void* o);
 /*--------------------------------------
 * Typedefs
@@ -32,13 +32,15 @@ typedef struct {
 * Local Variables
 ------------------------------------------*/
 static const struct smf_state led_states[] = {
-    [LED_ON_STATE] = SMF_CREATE_STATE(led_on_state_entry, led_on_state_run, NULL, NULL, NULL),
-    [LED_OFF_STATE] = SMF_CREATE_STATE(led_off_state_entry, led_off_state_run, NULL, NULL, NULL)
+    [LED_ON_STATE] = SMF_CREATE_STATE(NULL, led_on_state_run, led_on_state_exit, NULL, NULL),
+    [LED_OFF_STATE] = SMF_CREATE_STATE(NULL, led_off_state_run, led_off_state_exit, NULL, NULL)
 };
 static led_state_object_t led_state_object;
 
  void state_machine_init(){
+    printk("Changed as it should");
     led_state_object.count = 0;
+    LED_set(LED0, LED_ON);
     smf_set_initial(SMF_CTX(&led_state_object), &led_states[LED_ON_STATE]);
  }
 
@@ -46,8 +48,8 @@ static led_state_object_t led_state_object;
     return smf_run_state(SMF_CTX(&led_state_object));
  }
 
- static void led_on_state_entry(void* o){
-    LED_set(LED0, LED_ON);
+ static void led_on_state_exit(void* o){
+    LED_set(LED0, LED_OFF);
  }
 
  static enum smf_state_result led_on_state_run(void* o){
@@ -61,8 +63,8 @@ static led_state_object_t led_state_object;
     return SMF_EVENT_HANDLED;
  }
 
- static void led_off_state_entry(void* o){
-    LED_set(LED0, LED_OFF);
+ static void led_off_state_exit(void* o){
+    LED_set(LED0, LED_ON);
  }
 
  static enum smf_state_result led_off_state_run(void* o){
