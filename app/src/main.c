1bc2ea881c724ddb847e58b9f2f995505182802f
green-zinger <tristanjakob12@gmail.com>
2025-11-27 20:58:53 -0700

    This is the first commit for lesson 8, I had some issues that need to be resolved. Need  to reference original code for lesson 7 to make sure I coded the bluetooth stuff right.

--- app/src/main.c
+++ app/src/main.c
@@ -1,55 +1,83 @@
 /*
  * main.c
  */
-
-#include <inttypes.h>
-
+#include <stdio.h>
+#include <string.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/display.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/settings/settings.h>
 #include <zephyr/sys/printk.h>
-<<<<<<< HEAD
-#include <inttypes.h>
-#include "BTN.h"
-#include "LED.h"
-=======
 
-#include "BTN.h"
-#include "LED.h"
+BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef0);
+BT_DATA_BYTES(BT_DATA_FLAGS, flags);
+BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME));
 
-#define SLEEP_MS 1
->>>>>>> 411e7ce96d2adce819f520352c905a74509f0652
+static const struct bt_data ble_advertising_data[] = {
+  BT_DATA_BYTES(...),
+  BT_DATA(...),
+};
+static uint8_t ble_custom_characteristic_user_data[20] = {};
 
-#define SLEEP_TIME_MS 1
-
-<<<<<<< HEAD
-int main(void){
-
-    if(0>BTN_init()){
-        return 0;
-    }
-
-    if(0>LED_init()){
-        return 0;
-    }
-
-    while(1){
-        if(BTN_check_clear_pressed(BTN0)){
-            LED_toggle(LED0);
-            printk("Button 0 pressed!\n");
-        }
-        k_msleep(SLEEP_TIME_MS);
-    }
-    return 0;
-=======
-  if (0 > BTN_init()) {
-    return 0;
-  }
-  if (0 > LED_init()) {
-    return 0;
-  }
-
-  while(1) {
-    k_msleep(SLEEP_MS);
-  }
-	return 0;
->>>>>>> 411e7ce96d2adce819f520352c905a74509f0652
+static ssize_t ble_custom_characteristic_read_cb(struct bt_conn* conn, const struct bt_gatt_attr* attr,
+                                                 void* buf, uint16_t len, uint16_t offset) {
+  const char* value = attr->user_data;
+  return bt_gatt_attr_read(conn, attr, buf, len, offset, value, strlen(value));
 }
+
+static ssize_t ble_custom_characteristic_write_cb(struct bt_conn* conn, const struct bt_gatt_attr* attr,
+                                                  const void* buf, uint16_t len, uint16_t offset,
+                                                  uint8_t flags) {
+  uint8_t* value_ptr = attr->user_data;
+
+  if (offset + len > BLE_CUSTOM_CHARACTERISTIC_MAX_DATA_LENGTH) {
+    return BT_GATT_ERR(BT_ATT_ERR_INVALID_OFFSET);
+  }
+
+  memcpy(value_ptr + offset, buf, len);
+  value_ptr[offset + len] = 0;
+
+  return len;
+}
+static const struct bt_uuid_128 ble_custom_service_uuid = BT_UUID_INIT_128(BLE_CUSTOM_SERVICE_UUID);
+static const struct bt_uuid_128 ble_custom_characteristic_uuid = BT_UUID_INIT_128(BLE_CUSTOM_CHARACTERISTIC_UUID);
+
+BT_GATT_SERVICE_DEFINE(
+    ble_custom_service,  // Name of the struct that will store the config for this service
+    BT_GATT_PRIMARY_SERVICE(&ble_custom_service_uuid),  // Setting the service UUID
+    BT_GATT_CHARACTERISTIC(
+      &ble_custom_characteristic_uuid.uuid,  // Setting the characteristic UUID
+      <BT_GATT_CHRC_* values ORd together>,  // Possible operations
+      <BT_GATT_PERM_* values ORd together>,  // Permissions that connecting devices have
+      ble_custom_characteristic_read_cb,     // Callback for when this characteristic is read from
+      ble_custom_characteristic_write_cb,    // Callback for when this characteristic is written to
+      ble_custom_characteristic_user_data    // Initial data stored in this characteristic
+      ),
+);
+
+int main(void){
+  int enable_error = bt_enable(NULL);
+  int adv_error = bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, ble_advertising_data, ARRAY_SIZE(ble_advertising_data), NULL, 0);
+  if(0!=enable_error){
+    kprint("Error code: %d", enable_error)
+    return 0;
+  }
+      
+  if(0!=adv_error){
+    kprint("Error code %d", adv_error)
+    return 0;
+  }
+
+  while(1){
+    
+    
+  }
+}
+
